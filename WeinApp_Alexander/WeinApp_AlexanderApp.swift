5dbc5d21d398f98e950f15d273d9a1f1b59d7e57
Alexander <rucksal1@students.zhaw.ch>
2026-07-02 18:16:58 +0200

    Initial Commit

---
+++ WeinApp_Alexander/WeinApp_AlexanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeinApp_AlexanderApp.swift
+//  WeinApp_Alexander
+//
+//  Created by Alexander on 02.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeinApp_AlexanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
